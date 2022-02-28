a7ae06bc842d36069fcf2032ad20a62e176cf816
EC2 Default User <ec2-user@ip-172-31-61-153.ec2.internal>
2022-02-28 10:36:39 +0000

    finish

--- app/controllers/customers/comments_controller.rb
+++ app/controllers/customers/comments_controller.rb
@@ -1,31 +1,32 @@
 class Customers::CommentsController < ApplicationController
 
-def create
-    post = Post.find(params[:post_id])
-        comment = current_user.comments.new(comments_params)
-        comment.post_id = post.id
-    if comment.save!
-        flash[:notice] = "コメントしました！"
-        redirect_to post_path(post)
-    else
-        @comments = comment.all
-        render 'index'
+    def create
+        post = Post.find(params[:post_id])
+            comment = current_user.comments.new(comments_params)
+            comment.post_id = post.id
+        if comment.save!
+            flash[:notice] = "コメントしました！"
+            redirect_to post_path(post)
+        else
+            @comments = comment.all
+            render 'index'
+        end
     end
-end
-
-def destroy
-    post = Post.find(params[:post_id])
-    if Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-       flash[:notice] = "コメントを削除しました！"
-       redirect_to post_path(post)
-    else
-       render 'index'
-end
+    
+    def destroy
+        post = Post.find(params[:post_id])
+        if Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+           flash[:notice] = "コメントを削除しました！"
+           redirect_to post_path(post)
+        else
+           render 'index'
+        end 
+    end
 
 private
 
-def comments_params
-    params.require(:comment).permit(:text, :post_id).merge(user_id: current_user.id)
-end
+    def comments_params
+        params.require(:comment).permit(:text, :post_id).merge(user_id: current_user.id)
+    end
 
 end
