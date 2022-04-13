220e01116bb8b7d1032dea446c0cba1060fd12dd
fukuryo <ryohei.1998.05.09@gmail.com>
2022-04-13 14:14:43 +0000

    finish

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,24 +1,24 @@
 class Post < ApplicationRecord
 
-has_many :comments, dependent: :destroy
-belongs_to :user
-belongs_to :genre
-attachment :image
-has_many :likes, dependent: :destroy
-
-with_options presence: true do
-  validates :name
-  validates :introduction
-  validates :image
-  validates :genre_id
-end
-
-def self.search(keyword)
-  where(["name like? OR introduction like?", "%#{keyword}%", "%#{keyword}%"])
-end
-
-def liked_by?(user)
-    likes.where(user_id: user.id).exists?
-end
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  belongs_to :genre
+  attachment :image
+  has_many :likes, dependent: :destroy
+  
+  with_options presence: true do
+    validates :name
+    validates :introduction
+    validates :image
+    validates :genre_id
+  end
+  
+  def self.search(keyword)
+    where(["name like? OR introduction like?", "%#{keyword}%", "%#{keyword}%"])
+  end
+  
+  def liked_by?(user)
+      likes.where(user_id: user.id).exists?
+  end
 
 end
